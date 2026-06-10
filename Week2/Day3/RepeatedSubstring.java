142782f8d0cc6f30ee2a25888a2ef07c07f981bf
Anshika Gupta <anshikaguptaballia@gmail.com>
2026-06-11 00:36:39 +0530

    Checked if a string has repeated substring

---
+++ Week2/Day3/RepeatedSubstring.java
@@ -0,0 +1,39 @@
+package Week2.Day3;
+
+public class RepeatedSubstring {
+
+
+    public static boolean repeatedSubstringPattern(String s) {
+
+        int n = s.length();
+
+        for (int len = 1; len <= n / 2; len++) {
+
+            if (n % len == 0) {
+
+                String sub = s.substring(0, len);
+
+                StringBuilder sb = new StringBuilder();
+
+                int times = n / len;
+
+                for (int i = 0; i < times; i++) {
+                    sb.append(sub);
+                }
+
+                if (sb.toString().equals(s)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abab";
+
+        System.out.println(repeatedSubstringPattern(s));
+    }
+}
