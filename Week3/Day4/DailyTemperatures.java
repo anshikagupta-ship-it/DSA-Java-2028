8a48fa099c5c06dc32aa0eb0b618ceddbf1de02e
Anshika Gupta <anshikaguptaballia@gmail.com>
2026-06-20 02:53:36 +0530

    Solved daily temperature problem

---
+++ Week3/Day4/DailyTemperatures.java
@@ -0,0 +1,36 @@
+package Week3.Day4;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+        System.out.println(
+                Arrays.toString(dailyTemperatures(temperatures)));
+    }
+}
